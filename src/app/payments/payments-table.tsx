f7ff76495668e9c5487b64cc62bfe7ec529471a8
David Amankwaah <grehub24@gmail.com>
2025-09-09 22:03:17 +0000

    FIX "0 of 36 row(s) selected. Previous Next" TO SHOW THE NUMBER ITEMS ON

--- src/app/payments/payments-table.tsx
+++ src/app/payments/payments-table.tsx
@@ -95,8 +95,8 @@ export function PaymentsTable<TData, TValue>({
         </div>
         <div className="flex items-center justify-end space-x-2 py-4">
              <div className="flex-1 text-sm text-muted-foreground">
-                {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                {table.getFilteredRowModel().rows.length} row(s) selected.
+                Page {table.getState().pagination.pageIndex + 1} of{" "}
+                {table.getPageCount()}
             </div>
             <Button
                 variant="outline"
--- src/app/students/data-table.tsx
+++ src/app/students/data-table.tsx
@@ -237,25 +237,30 @@ export function DataTable({
         </div>
         <div className="flex items-center justify-end space-x-2 py-4">
             <div className="flex-1 text-sm text-muted-foreground">
-                {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                {table.getFilteredRowModel().rows.length} row(s) selected.
+                Total {table.getFilteredRowModel().rows.length} row(s).
             </div>
-            <Button
-                variant="outline"
-                size="sm"
-                onClick={() => table.previousPage()}
-                disabled={!table.getCanPreviousPage()}
-            >
-                Previous
-            </Button>
-            <Button
-                variant="outline"
-                size="sm"
-                onClick={() => table.nextPage()}
-                disabled={!table.getCanNextPage()}
-            >
-                Next
-            </Button>
+            <div className="flex items-center space-x-2">
+                <p className="text-sm font-medium">
+                    Page {table.getState().pagination.pageIndex + 1} of{" "}
+                    {table.getPageCount()}
+                </p>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    Previous
+                </Button>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                >
+                    Next
+                </Button>
+            </div>
         </div>
       </CardContent>
     </Card>
